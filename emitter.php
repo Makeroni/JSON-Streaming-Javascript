32bc21b5b99d3052330b254475abbc0648425466
B0RJA <borja.latorre@eead.csic.es>
2014-03-31 10:04:19 +0200

    First commit

---
+++ emitter.php
@@ -0,0 +1,40 @@
+<?
+# We start with the standard headers. PHP allows us this much
+header("Cache-Control: no-cache");
+header("Cache-Control: private");
+header("Pragma: no-cache");
+header("Content-Type: application/json");
+
+# Set this so PHP doesn't timeout during a long stream
+set_time_limit(0);
+
+# Disable Apache and PHP's compression of output to the client
+@apache_setenv('no-gzip', 1);
+@ini_set('zlib.output_compression', 0);
+
+# Set implicit flush, and flush all current buffers
+@ini_set('implicit_flush', 1);
+for ($i = 0; $i < ob_get_level(); $i++)
+    ob_end_flush();
+ob_implicit_flush(1);
+
+$msg = "{\n   \"led\": [\n";
+echo $msg;
+
+# The loop
+$i=0;
+while (true) {
+
+    $red   = (int)(sin($i*2*pi()/128 + 0) * 127 + 128);
+    $green = (int)(sin($i*2*pi()/128 + 2) * 127 + 128);
+    $blue  = (int)(sin($i*2*pi()/128 + 4) * 127 + 128);
+    $msg = "      {\"red\":$red, \"green\":$green, \"blue\":$blue},\n";
+    echo $msg;
+
+    $i = $i + 2;
+    if ($i==128) {
+		$i = 0;
+	}
+    usleep(50000); # 50ms between messages
+}
+?>
